560f07faf735ef33b32d5d90197c90344689a731
Tanthan2k <110687308+Tanthan2k@users.noreply.github.com>
2023-03-31 23:22:44 +0700

    birthday

---
+++ thiep-mung/config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'Bé báo con <3';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Em tuổi mới thành công và tràn đầy niềm vui nhé! Sinh nhật vui vẻ <33.';
+
+
+
+const giftImage = 'phuong-anh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
